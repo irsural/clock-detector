5541b4a33ad3f38d04a92cdd83246a91e44d9b77
qucals <totalboy@outlook.com>
2020-12-07 03:39:59 +0500

    added test on readable

---
+++ src/tests/test_reading.py
@@ -0,0 +1,21 @@
+import cv2
+import unittest
+import utility
+
+utility.settings_sys_path()
+
+from detector import time_reader as tr
+
+class TestReadTime(unittest.TestCase):
+
+    def test_readable(self):
+        list_images = utility.get_list_images()
+
+        for image in list_images:
+            _image = cv2.imread(image)
+            reader = tr.TimeReader(_image)
+
+            self.assertIsNotNone(reader.read())
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/tests/utility.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+def settings_sys_path():
+    need_dir = 'src'
+    current_path = os.path.abspath(os.curdir)
+    
+    while os.path.basename(current_path) != need_dir:
+        os.chdir('..')
+        current_path = os.path.abspath(os.curdir)
+
+    sys.path[0] = current_path
+
+def get_list_images():
+    list_images = []
+
+    project_name = 'clock-detector'
+    cache_name = 'cache'
+    current_path = os.path.abspath(os.curdir)
+
+    while os.path.basename(current_path) != project_name:
+        os.chdir('..')
+        current_path = os.path.abspath(os.curdir)
+
+    current_path = os.path.join(current_path, cache_name)
+
+    for directory in os.listdir(current_path):
+        dir_path = os.path.join(current_path, directory)
+
+        for image in os.listdir(dir_path):
+            image_path = os.path.join(dir_path, image)
+            list_images.append(os.path.abspath(image_path))
+
+    return list_images
